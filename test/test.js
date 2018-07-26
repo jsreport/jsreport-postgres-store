0ac3a98e7c2471ecb5f07d3d182b4d8baf43a617
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-07-26 13:17:04 +0200

    close pool when closing jsreport and add common store tests

---
+++ test/test.js
@@ -0,0 +1,31 @@
+require('should')
+process.env.DEBUG = 'jsreport'
+const jsreport = require('jsreport-core')
+
+describe('common store tests', () => {
+  let reporter
+
+  beforeEach(async () => {
+    reporter = jsreport({
+      store: { provider: 'postgres' }
+    })
+      .use(require('../')({
+        'host': 'localhost',
+        'port': 5432,
+        'database': 'jsreport',
+        'user': 'postgres',
+        'password': 'password'
+      }))
+    await reporter.init()
+
+    const drop = reporter.documentStore.provider.drop.bind(reporter.documentStore.provider)
+    reporter.documentStore.provider.drop = async () => {
+      await drop()
+      return reporter.documentStore.init()
+    }
+  })
+
+  afterEach(() => reporter.close())
+
+  jsreport.tests.documentStore()(() => reporter.documentStore)
+})
